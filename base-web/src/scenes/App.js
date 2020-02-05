e09fc6a89c88d5e8ad0d2dfb5c958cb98bc9b6ca
Aluno Bandtec <github@alunos.bandtec.com.br>
2020-02-04 22:11:44 -0200

    Update stylesheet

---
+++ base-web/src/scenes/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// MARK: - SCENES
+import Intro from "../scenes/onboarding/intro/intro"
+
+// 
+import "../resource/style/baseStyle.css"
+
+function App() {
+  return (
+    function App() {
+      return (
+        <BrowserRouter>
+          <Route path="/" exact component={Home} />
+        </BrowserRouter>
+      );
+    }
+  );
+}
+
+export default App;
---
+++ base-web/src/scenes/onboarding/intro/intro.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+
